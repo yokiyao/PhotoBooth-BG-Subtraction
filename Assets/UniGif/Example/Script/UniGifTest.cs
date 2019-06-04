743bad10f921242563dde475ed571f8dea3d12b0
yoki.yao <yoki.yao@yucolab.com>
2019-06-04 17:30:15 +0800

    packages with all imgs uploaded

---
+++ Assets/UniGif/Example/Script/UniGifTest.cs
@@ -0,0 +1,37 @@
+﻿/*
+UniGif
+Copyright (c) 2015 WestHillApps (Hironari Nishioka)
+This software is released under the MIT License.
+http://opensource.org/licenses/mit-license.php
+*/
+
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UniGifTest : MonoBehaviour
+{
+    //[SerializeField]
+    //private InputField m_inputField;
+    [SerializeField]
+    public UniGifImage m_uniGifImage;
+
+    private bool m_mutex;
+
+    public void StartLoadGif(string gifpath)
+    {
+        if (m_mutex || m_uniGifImage == null || string.IsNullOrEmpty(gifpath))
+        {
+            return;
+        }
+
+        m_mutex = true;
+        StartCoroutine(ViewGifCoroutine(gifpath));
+    }
+
+    private IEnumerator ViewGifCoroutine(string gifpath)
+    {
+        yield return StartCoroutine(m_uniGifImage.SetGifFromUrlCoroutine(gifpath));
+        m_mutex = false;
+    }
+}
